d0be951597d462b4a84159dc409dd43c2f6a94ab
notoliver <oliver.qiu@hotmail.com>
2022-07-25 16:03:27 -0400

    init gallery

---
+++ assets/data.js
@@ -0,0 +1,84 @@
+var home = [
+    {
+        module: 'responsivegallery',
+        id: 'walkingmenorca',
+        targetHeight: 700,
+        offset: 0,
+        filenames: [
+            {
+                image: 'DSCF8081.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8109.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8111.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8120.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8127.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8133.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8144.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8146.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8290.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8313.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8316.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8325.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8327.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: 'DSCF8329.jpg',
+                x: '2',
+                y: '3'
+            }
+        ]
+    },
+    {
+        module: 'spacer',
+        height: '10px'
+    }
+]
