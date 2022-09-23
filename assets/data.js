99e0a431551f707e728892c101bd9aaaabcd388d
notoliver <47568829+notoliver@users.noreply.github.com>
2022-09-23 15:38:19 -0700

    bio and lisbon and working sidemenu

--- assets/data.js
+++ assets/data.js
@@ -72,3 +72,90 @@ var home = [
         height: '10px'
     }
 ]
+
+var lisbon = [
+    {
+        module: 'responsivegallery',
+        id: 'lisbon2022',
+        targetHeight: 700,
+        offset: 0,
+        filenames: [
+            {
+                image: '1.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '2.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '3.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '4.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: '5.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: '11.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '6.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '7.jpg',
+                x: '3',
+                y: '2'
+            }, 
+            {
+                image: '10.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '8.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '9.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '12.jpg',
+                x: '3',
+                y: '2'
+            }
+        ]
+    },
+    {
+        module: 'spacer',
+        height: '10px'
+    }
+]
+
+var bio = [
+    {
+        module:"description",
+        text:"There is nothing sweeter in life than Cinestill. <br>Hey I'm Fynn, a 16 year old film photographer from the UK. <br>This website acts as a gallery for all of my favorite photos I have taken across Europe.",
+        inline:"style=\'text-align:left\'"
+    },
+    {
+        module: 'spacer',
+        height: '10px'
+    }
+]
--- assets/scripts/sidemenu.js
+++ assets/scripts/sidemenu.js
@@ -1,5 +1,54 @@
 var text='';
 
+var active = document.getElementById("navmenus").getAttribute("active");
+console.log(active);
+
+//var pages = ["home","lisbon","bio"];
+
+/*
+<div class='sidemenu'>
+    <a href="/" style="font-size:30px; padding-bottom:24px;">Fynn Dyer</a>
+    <br><br>
+    <a href="/" pp="pp">Walking Menorca</a>
+    <br>
+    <a href="/lisbon" >Streets of Lisbon</a>
+    <br>
+    <a href="/bio">Bio</a>
+</div>
+
+<div class='mobilemenu'>
+
+    <div>
+        <a href="/">Fynn Dyer</a>
+        <span style="float:right;" onclick="openNavModal()">
+            menu 
+        </span>
+    </div>
+
+    <div class="navmodal" id="myNavModal">
+        <span class="close cursor" onclick="closeNavModal()">&times;</span>
+        <div class="navmodal-content">
+            <a href="/" pp="pp">Walking Menorca</a>
+            <br>
+            <a href="/lisbon" >Streets of Lisbon</a>
+            <br>
+            <a href="/bio">Bio</a>
+        </div>
+    </div>
+
+</div>
+*/
 
 
-//document.getElementsByClassName('sidemenu')[0].innerHTML = text;
+text+='<div class=\'sidemenu\'>\r\n    <a href=\"\/\" style=\"font-size:30px; padding-bottom:24px;\">Fynn Dyer<\/a>\r\n    <br><br>\r\n    <a href=\"\/\" pp=\"pp\">Walking Menorca<\/a>\r\n    <br>\r\n    <a href=\"\/lisbon\" >Streets of Lisbon<\/a>\r\n    <br>\r\n    <a href=\"\/bio\">Bio<\/a>\r\n<\/div>\r\n\r\n<div class=\'mobilemenu\'>\r\n\r\n    <div>\r\n        <a href=\"\/\">Fynn Dyer<\/a>\r\n        <span style=\"float:right;\" onclick=\"openNavModal()\">\r\n            menu \r\n        <\/span>\r\n    <\/div>\r\n\r\n    <div class=\"navmodal\" id=\"myNavModal\">\r\n        <span class=\"close cursor\" onclick=\"closeNavModal()\">&times;<\/span>\r\n        <div class=\"navmodal-content\">\r\n            <a href=\"\/\" pp=\"pp\">Walking Menorca<\/a>\r\n            <br>\r\n            <a href=\"\/lisbon\" >Streets of Lisbon<\/a>\r\n            <br>\r\n            <a href=\"\/bio\">Bio<\/a>\r\n        <\/div>\r\n    <\/div>\r\n\r\n<\/div>'
+
+var text2=text;
+
+if(active!=="home"){
+    var results = getIndicesOf(active, text,true);
+    var firstoccurance=results[0]+active.length+1;
+    var secondoccurance=results[1]+active.length+1;
+    text2 = text.slice(0, firstoccurance) + " class=\"active\" " + text.slice(firstoccurance,secondoccurance)+ " class=\"active\" " +text.slice(secondoccurance);
+}
+
+document.getElementsByClassName('navmenus')[0].innerHTML = text2;
