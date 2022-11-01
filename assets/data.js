2b3446369f152f34f439cf6204463ab250d9d8ea
notoliver <oliver.qiu@hotmail.com>
2022-11-01 13:54:25 -0700

    uk

--- assets/data.js
+++ assets/data.js
@@ -148,6 +148,92 @@ var lisbon = [
     }
 ]
 
+var uk = [
+    {
+        module: 'responsivegallery',
+        id: 'aroundtheuk',
+        targetHeight: 700,
+        offset: 0,
+        filenames: [
+            {
+                image: '1.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '2.jpg',
+                x: '2',
+                y: '3'
+            },
+            {
+                image: '3.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '4.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '5.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '11.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '6.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '7.jpg',
+                x: '3',
+                y: '2'
+            }, 
+            {
+                image: '10.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '8.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '9.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '12.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '13.jpg',
+                x: '3',
+                y: '2'
+            },
+            {
+                image: '14.jpg',
+                x: '3',
+                y: '2'
+            }
+        ]
+    },
+    {
+        module: 'spacer',
+        height: '10px'
+    }
+]
+
+
 var bio = [
     {
         module:"description",
--- assets/scripts/sidemenu.js
+++ assets/scripts/sidemenu.js
@@ -3,7 +3,7 @@ var text='';
 var active = document.getElementById("navmenus").getAttribute("active");
 console.log(active);
 
-//var pages = ["home","lisbon","bio"];
+//var pages = ["home","lisbon", "uk","bio"];
 
 /*
 <div class='sidemenu'>
@@ -12,6 +12,8 @@ console.log(active);
     <a href="/" pp="pp">Walking Menorca</a>
     <br>
     <a href="/lisbon" >Streets of Lisbon</a>
+    <br>
+    <a href="/uk" pp="pp">Around the UK</a>
     <br>
     <a href="/bio">Bio</a>
 </div>
@@ -32,6 +34,8 @@ console.log(active);
             <br>
             <a href="/lisbon" >Streets of Lisbon</a>
             <br>
+            <a href="/uk" pp="pp">Around the UK</a>
+            <br>
             <a href="/bio">Bio</a>
         </div>
     </div>
@@ -40,7 +44,7 @@ console.log(active);
 */
 
 
-text+='<div class=\'sidemenu\'>\r\n    <a href=\"\/\" style=\"font-size:30px; padding-bottom:24px;\">Fynn Dyer<\/a>\r\n    <br><br>\r\n    <a href=\"\/\" pp=\"pp\">Walking Menorca<\/a>\r\n    <br>\r\n    <a href=\"\/lisbon\" >Streets of Lisbon<\/a>\r\n    <br>\r\n    <a href=\"\/bio\">Bio<\/a>\r\n<\/div>\r\n\r\n<div class=\'mobilemenu\'>\r\n\r\n    <div>\r\n        <a href=\"\/\">Fynn Dyer<\/a>\r\n        <span style=\"float:right;\" onclick=\"openNavModal()\">\r\n            menu \r\n        <\/span>\r\n    <\/div>\r\n\r\n    <div class=\"navmodal\" id=\"myNavModal\">\r\n        <span class=\"close cursor\" onclick=\"closeNavModal()\">&times;<\/span>\r\n        <div class=\"navmodal-content\">\r\n            <a href=\"\/\" pp=\"pp\">Walking Menorca<\/a>\r\n            <br>\r\n            <a href=\"\/lisbon\" >Streets of Lisbon<\/a>\r\n            <br>\r\n            <a href=\"\/bio\">Bio<\/a>\r\n        <\/div>\r\n    <\/div>\r\n\r\n<\/div>'
+text+='<div class=\'sidemenu\'>\r\n    <a href=\"\/\" style=\"font-size:30px; padding-bottom:24px;\">Fynn Dyer<\/a>\r\n    <br><br>\r\n    <a href=\"\/\" pp=\"pp\">Walking Menorca<\/a>\r\n    <br>\r\n    <a href=\"\/lisbon\" >Streets of Lisbon<\/a>\r\n    <br>\r\n    <a href=\"\/uk\" pp=\"pp\">Around the UK<\/a>\r\n    <br>\r\n    <a href=\"\/bio\">Bio<\/a>\r\n<\/div>\r\n\r\n<div class=\'mobilemenu\'>\r\n\r\n    <div>\r\n        <a href=\"\/\">Fynn Dyer<\/a>\r\n        <span style=\"float:right;\" onclick=\"openNavModal()\">\r\n            menu \r\n        <\/span>\r\n    <\/div>\r\n\r\n    <div class=\"navmodal\" id=\"myNavModal\">\r\n        <span class=\"close cursor\" onclick=\"closeNavModal()\">&times;<\/span>\r\n        <div class=\"navmodal-content\">\r\n            <a href=\"\/\" pp=\"pp\">Walking Menorca<\/a>\r\n            <br>\r\n            <a href=\"\/lisbon\" >Streets of Lisbon<\/a>\r\n            <br>\r\n            <a href=\"\/uk\" pp=\"pp\">Around the UK<\/a>\r\n            <br>\r\n            <a href=\"\/bio\">Bio<\/a>\r\n        <\/div>\r\n    <\/div>\r\n\r\n<\/div>'
 
 var text2=text;
 
