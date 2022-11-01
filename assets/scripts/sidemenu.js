f2ae5112c8ad9fd758e5367aa6a0b3721e410598
notoliver <oliver.qiu@hotmail.com>
2022-11-01 14:15:42 -0700

    pp

--- assets/scripts/sidemenu.js
+++ assets/scripts/sidemenu.js
@@ -13,7 +13,7 @@ console.log(active);
     <br>
     <a href="/lisbon" >Streets of Lisbon</a>
     <br>
-    <a href="/uk" pp="pp">Around the UK</a>
+    <a href="/uk" >Around the UK</a>
     <br>
     <a href="/bio">Bio</a>
 </div>
@@ -34,7 +34,7 @@ console.log(active);
             <br>
             <a href="/lisbon" >Streets of Lisbon</a>
             <br>
-            <a href="/uk" pp="pp">Around the UK</a>
+            <a href="/uk" >Around the UK</a>
             <br>
             <a href="/bio">Bio</a>
         </div>
@@ -44,7 +44,7 @@ console.log(active);
 */
 
 
-text+='<div class=\'sidemenu\'>\r\n    <a href=\"\/\" style=\"font-size:30px; padding-bottom:24px;\">Fynn Dyer<\/a>\r\n    <br><br>\r\n    <a href=\"\/\" pp=\"pp\">Walking Menorca<\/a>\r\n    <br>\r\n    <a href=\"\/lisbon\" >Streets of Lisbon<\/a>\r\n    <br>\r\n    <a href=\"\/uk\" pp=\"pp\">Around the UK<\/a>\r\n    <br>\r\n    <a href=\"\/bio\">Bio<\/a>\r\n<\/div>\r\n\r\n<div class=\'mobilemenu\'>\r\n\r\n    <div>\r\n        <a href=\"\/\">Fynn Dyer<\/a>\r\n        <span style=\"float:right;\" onclick=\"openNavModal()\">\r\n            menu \r\n        <\/span>\r\n    <\/div>\r\n\r\n    <div class=\"navmodal\" id=\"myNavModal\">\r\n        <span class=\"close cursor\" onclick=\"closeNavModal()\">&times;<\/span>\r\n        <div class=\"navmodal-content\">\r\n            <a href=\"\/\" pp=\"pp\">Walking Menorca<\/a>\r\n            <br>\r\n            <a href=\"\/lisbon\" >Streets of Lisbon<\/a>\r\n            <br>\r\n            <a href=\"\/uk\" pp=\"pp\">Around the UK<\/a>\r\n            <br>\r\n            <a href=\"\/bio\">Bio<\/a>\r\n        <\/div>\r\n    <\/div>\r\n\r\n<\/div>'
+text+='<div class=\'sidemenu\'>\r\n    <a href=\"\/\" style=\"font-size:30px; padding-bottom:24px;\">Fynn Dyer<\/a>\r\n    <br><br>\r\n    <a href=\"\/\" pp=\"pp\">Walking Menorca<\/a>\r\n    <br>\r\n    <a href=\"\/lisbon\" >Streets of Lisbon<\/a>\r\n    <br>\r\n    <a href=\"\/uk\" >Around the UK<\/a>\r\n    <br>\r\n    <a href=\"\/bio\">Bio<\/a>\r\n<\/div>\r\n\r\n<div class=\'mobilemenu\'>\r\n\r\n    <div>\r\n        <a href=\"\/\">Fynn Dyer<\/a>\r\n        <span style=\"float:right;\" onclick=\"openNavModal()\">\r\n            menu \r\n        <\/span>\r\n    <\/div>\r\n\r\n    <div class=\"navmodal\" id=\"myNavModal\">\r\n        <span class=\"close cursor\" onclick=\"closeNavModal()\">&times;<\/span>\r\n        <div class=\"navmodal-content\">\r\n            <a href=\"\/\" pp=\"pp\">Walking Menorca<\/a>\r\n            <br>\r\n            <a href=\"\/lisbon\" >Streets of Lisbon<\/a>\r\n            <br>\r\n            <a href=\"\/uk\" >Around the UK<\/a>\r\n            <br>\r\n            <a href=\"\/bio\">Bio<\/a>\r\n        <\/div>\r\n    <\/div>\r\n\r\n<\/div>'
 
 var text2=text;
 
